b8c92b4cea00d153601f99e115ee047837479c27
home <home@LAPTOP-CLEMU8AQ>
2016-09-18 23:13:22 +0530

    committed

---
+++ WelcomeSaiScala/src/Test.scala
@@ -0,0 +1,14 @@
+
+
+object Test {
+  /* This is my first java program.  
+   * This will print 'Hello World' as the output
+   */
+  def main(args: Array[String]) {
+    println("Hello, world!") // prints Hello World
+    
+    
+    println("Hello, world! " + args.length)
+    println()
+  }
+}
